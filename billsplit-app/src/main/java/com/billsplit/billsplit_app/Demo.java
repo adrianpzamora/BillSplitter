b0cb7e2e71d0596b429ef4a6e84cbaffb7ce506d
Adrian Zamora <adrianpzamora@yahoo.com>
2019-01-10 19:28:13 -0800

    Demo class with main to test Billsplitter class

---
+++ billsplit-app/src/main/java/com/billsplit/billsplit_app/Demo.java
@@ -0,0 +1,12 @@
+package com.billsplit.billsplit_app;
+
+public class Demo {
+
+	public static void main(String[] args) {
+		Billsplitter billsplitter = new Billsplitter();
+		
+		billsplitter.displayMainMenu();
+
+	}
+
+}
